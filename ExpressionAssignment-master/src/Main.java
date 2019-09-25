6bc7de25e4a81cde5f48a6cba21bd745ab0edfa3
MattyPham <55457401+MattyPham@users.noreply.github.com>
2019-09-25 11:20:45 -0700

    Expressions Assignment
    Matthew's Assignment

---
+++ ExpressionAssignment-master/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String args[]){
+        /*For the following expressions write out using code step by step how the
+        compiler will evaluate it
+        Example:
+        System.out.println(2 * 5 + 3);
+        System.out.println(10 + 3);
+        System.out.println(13);
+        Only complete one computation per line of code
+        The output for every line is always equal to the same number
+        In example above the out put should be
+        13
+        13
+        13
+        */
+        //Expression 1
+        System.out.println(10 - 8);
+
+        //Expression 2
+        System.out.println(-6 + 8);
+
+        //Expression 3
+        System.out.println(0.5 * 4);
+
+        //Expression 4
+        System.out.println(198765435 / 99382717.5);
+
+        //Expression 5
+        System.out.println(((60 + 40) * (140 - 90)) / 2500);
+    }
+}
